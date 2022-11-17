fcd689fb161e6fc725cd1560a17f5bf5387d973a
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-11-17 14:55:09 +0000

    added underage test

--- pub_checker_start/src/main/java/Guest.java
+++ pub_checker_start/src/main/java/Guest.java
@@ -2,10 +2,12 @@ public class Guest {
 
     private String name;
     private int age;
+    private double wallet;
 
     public Guest(String name, int age){
         this.name = name;
         this.age = age;
+        this.wallet = 0;
     }
 
     // getters and setters
@@ -21,7 +23,15 @@ public class Guest {
         return this.age;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
+    }
+
+    public double getWallet() {
+        return this.wallet;
+    }
+
+    public void setWallet(double wallet){
+        this.wallet = wallet;
     }
 }
--- pub_checker_start/src/test/java/ServerTest.java
+++ pub_checker_start/src/test/java/ServerTest.java
@@ -7,24 +7,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ServerTest {
 
     Server server;
+    Guest steve;
+
 
     @BeforeEach
     public void setUp(){
         server = new Server();
+        steve = new Guest("Steve", 20);
     }
 
     // TODO: test that guest can only get served if over 18
     @Test
     public void canServeGuestOver18() {
-        // Constructs a new guest and sets their age to 20 (>18)
-        Guest steve = new Guest("Steve", 20);
-
         // Uses method to check if guest can be served
         boolean canServe = server.canServeGuest(steve);
         assertThat(canServe).isEqualTo(true);
     }
-    // TODO: test that guest can only get served if has enough money to buy a drink (every drink is £5)
 
+    @Test
+    public void canNOTServeGuestOver18(){
+        // sets guest's age to <18
+        steve.setAge(17);
+        assertThat(server.canServeGuest(steve)).isEqualTo(false);
+    }
+
+    // TODO: test that guest can only get served if has enough money to buy a drink (every drink is £5)
+    @Test
+    public void canServeGuestIfTheyHaveEnoughMoney(){
+
+    }
     // TODO: test that guest can only get served if sober enough (set sobriety level on guest)
 
     // TODO: test that guest can only get served if guest is not banned from the pub
